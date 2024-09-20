7bb0b1d9bd8bbb160c6a4a38a46f46e9f0d17201
domaksym69@gmail.com <domaksym69@gmail.com>
2024-09-20 20:11:49 +0300

    Add project files.

---
+++ four/four.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //1 task
+    /*int a;
+    cin >> a;
+    int sum = 0;
+    for (int i = a; i < 501; i++) {
+        sum += a;
+    }
+    cout << sum;*/
+
+
+    //2 task
+    /*int x, y;
+    int sum = 1;
+    cin >> x;
+    cin >> y;
+    for (int i = 0; i < y; i++) {
+        sum *= x;
+    }
+    cout << sum;*/
+
+
+    //3 task
+    /*float sum = 0;
+    for (int i = 1; i < 1001; i++) {
+        sum += i;
+    }
+    cout << sum / 1000;*/
+
+
+    //4 task
+    /*int a;
+    cin >> a;
+    int dob = 1;
+    for (int i = a; i < 21; i++) {
+        dob *= i;
+    }
+    cout << dob;*/
+
+
+    //5 task
+    /*int a;
+    cin >> a;
+    for (int i = 1; i < 11; i++) {
+        cout << a << "+" << i << "=" << a * i << endl;
+    }*/
+
+
+    //6 task
+    /*int sum = 0;
+    int a, b, c;
+    for (int i = 100; i < 1000; i++) {
+        a = i / 100;
+        b = (i / 10) % 10;
+        c = i % 10;
+        if (a == b || b == c || c == a) {
+            sum++;
+        }
+    }
+    cout << sum;*/
+
+
+    //7 task
+    /*int sum = 0;
+    int a, b, c;
+    for (int i = 100; i < 1000; i++) {
+        a = i / 100;
+        b = (i / 10) % 10;
+        c = i % 10;
+        if (a == b || b == c || c == a) {
+            continue;
+        }
+        else {
+            sum++;
+        }
+    }
+    cout << sum;*/
+}
